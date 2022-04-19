1b4a5ec0e9047ea5570c6fbee030f9eaef941d7a
pm-rm <metscher@rhinos-media.de>
2022-04-20 00:06:05 +0200

    Added theme for the first task

---
+++ custom/plugins/Task1/src/Task1.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Task1;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Task1 extends Plugin implements ThemeInterface
+{
+}
